f0fb0ac3056c2a70d4feb13f41997bcbe39b69e0
中西直孝 <nakanishi-naotaka@gr.sei.co.jp>
2018-06-13 11:17:51 +0900

    Project テーブルの API を実装

--- Server/admin.py
+++ Server/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+    #  list_display = ('id', 'title',)       # 一覧に表示したい項目
+    #  list_display_links = ('id', )  # リンクを表示する項目
--- Server/models.py
+++ Server/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.TextField(blank=False)
+    description = models.TextField(blank=True)
+    owner_id = models.IntegerField(blank=False)
+    status = models.IntegerField(default=0, blank=False)
+
+    def __str__(self):
+        return self.title
+
+
