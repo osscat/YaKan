8ed92c890d86bf1b44e51716b18181336217ee0b
masuda <masuda-kazuya@gr.sei.co.jp>
2018-06-13 16:40:58 +0900

    プロジェクト追加フォームのコンポーネントを作成

---
+++ Client/src/components/AddProjectForm.vue
@@ -0,0 +1,35 @@
+/**
+ *  プロジェクトを追加するフォームのコンポーネント。
+ */
+<template>
+  <form>
+    タイトル<input type="text" id="title" />
+    <button type="button" @click="addProject">
+      登録
+    </button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+
+const PROJECT_POST_URL = 'http://127.0.0.1:8000/api/projects/'
+
+export default {
+  name: 'AddProjectForm',
+  methods: {
+    addProject: function () {
+      axios
+        .post(PROJECT_POST_URL, {
+          title: document.getElementById('title').value,
+          owner_id: 3
+        })
+        .then(response => { this.$parent.projects.push(response.data) })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
