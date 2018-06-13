2e67d87c7540da65c358b5f051ac3f1ea1ab1880
aa190038 <kawajiri-yasuo@gr.sei.co.jp>
2018-06-13 13:33:44 +0900

    api用のルーティング設定を追加

--- YaKan/urls.py
+++ YaKan/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+
+from Server.urls import router as server_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/', include(server_router.urls)),
 ]
