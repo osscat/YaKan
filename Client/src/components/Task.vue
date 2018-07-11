49bf4ab322e53355d8e3b2e8acac73e06ffeadaf
webstry <kawajiri-yasuo@osscat.net>
2018-07-11 09:54:32 +0900

    element-uiに置き換え 詳細部分の開閉ボタンを追加

--- Client/src/components/Task.vue
+++ Client/src/components/Task.vue
@@ -2,29 +2,55 @@
  *  タスクを表示するコンポーネント。
  */
 <template>
-  <div class="flex-container">
-    id:{{ task.id }}<br>
-    title:{{ task.title }}<br>
-    user_id:{{ task.user_id }}<br>
-    man_day:{{ task.man_day }}<br>
-    status:{{ task.status }}
+  <div>
+    <el-card class="task" :body-style="bodyStyle">
+      <div slot="header" class="header">
+        <span>{{task.title}}</span>
+        <el-button class="togglebutton" type="text" @click="toggle">
+          <i class="el-icon-arrow-down"></i>
+        </el-button>
+      </div>
+      <div id="detail">
+        id:{{ task.id }}<br>
+        title:{{ task.title }}<br>
+        user_id:{{ task.user_id }}<br>
+        man_day:{{ task.man_day }}<br>
+        status:{{ task.status }}
+      </div>
+     </el-card>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Task',
-  props: ['task']
+  props: ['task'],
+  data () {
+    return {
+      bodyStyle: {
+        display: null
+      }
+    }
+  },
+  mounted () {
+    this.bodyStyle.display = 'none'
+  },
+  methods: {
+    toggle: function () {
+      if (this.bodyStyle.display === 'none') {
+        this.bodyStyle.display = 'block'
+      } else {
+        this.bodyStyle.display = 'none'
+      }
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.flex-container {
-  margin: auto;
-  width: 85%;
-  height: 150px;
-  padding: 10px;
-  background-color: whitesmoke;
+.togglebutton {
+  float: right;
+  padding: 3px 0;
 }
 </style>
