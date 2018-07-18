1a7b2acd07abe2d7b3e464eab3c87313d05421cb
aa140043 <masuda-kazuya@gr.sei.co.jp>
2018-07-18 11:09:33 +0900

    環境変数を追加

--- Client/config/dev.env.js
+++ Client/config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  SERVER_ADDRESS: '"127.0.0.1:8080"',
+  API_BASE_URL: '"http://127.0.0.1:8000"'
 })
