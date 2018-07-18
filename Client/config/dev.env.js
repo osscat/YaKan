674e67786bb7ae96d3c8e66b13c5c12c7b7a49b4
aa140043 <masuda-kazuya@gr.sei.co.jp>
2018-07-18 13:58:09 +0900

    WebSocket のサーバーアドレスのポートが間違っていたので修正

--- Client/config/dev.env.js
+++ Client/config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  SERVER_ADDRESS: '"127.0.0.1:8080"',
+  SERVER_ADDRESS: '"localhost:8000"',
   API_BASE_URL: '"http://127.0.0.1:8000"'
 })
