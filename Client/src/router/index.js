31c9d2b22ae721a801b3c9c60f27a981d73e6f76
Naotaka Nakanishi <nakanishi-naotaka@osscat.net>
2018-07-18 15:06:38 +0900

    ユーザー管理画面を追加

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Top from '@/components/Top'
+import UserAdmin from '@/components/UserAdmin'
 import Login from '@/components/Login'
 
 Vue.use(Router)
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Top',
       component: Top
+    },
+    {
+      path: '/userAdmin',
+      name: 'UserAdmin',
+      component: UserAdmin
     },
     {
       path: '/login',
