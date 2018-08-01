0204ed9a33e478171b4e7fd4345fe09353b8e7ef
Naotaka Nakanishi <nakanishi-naotaka@osscat.net>
2018-08-01 13:30:30 +0900

    ログアウトとパスワード変更を実装

---
+++ Client/src/components/ChangePasswordForm.vue
@@ -0,0 +1,103 @@
+/**
+ *  パスワード変更画面コンポーネント。
+ */
+<template>
+  <el-dialog title="パスワード変更" width="500px" :visible.sync="dialogVisible" @closed="onClosed">
+    <el-alert v-for="(error, index) in errors" :key="index" :title="error" type="error"></el-alert>
+    <el-form :model="form" ref="form" label-width="180px">
+      <el-form-item label="新パスワード" :rules="rules" prop="new_password1">
+        <el-input type="password" v-model="form.new_password1"></el-input>
+      </el-form-item>
+      <el-form-item label="新パスワード（確認）" :rules="rules" prop="new_password2">
+        <el-input type="password" v-model="form.new_password2"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="onClosed">キャンセル</el-button>
+      <el-button type="primary" @click="save">保存</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+import axios from 'axios'
+
+const URL = process.env.API_BASE_URL + '/rest-auth/password/change/'
+
+export default {
+  name: 'ChangePasswordForm',
+  props: ['visible'],
+  data () {
+    return {
+      errors: [],
+      dialogVisible: this.visible,
+      form: {
+        new_password1: null,
+        new_password2: null
+      },
+      rules: [
+        {
+          required: true,
+          message: '入力してください',
+          trigger: 'blur'
+        },
+        {
+          min: 8,
+          message: '8文字以上にしてください',
+          trigger: 'blur'
+        }
+      ]
+    }
+  },
+  watch: {
+    visible: function (value) {
+      this.dialogVisible = value
+    }
+  },
+  methods: {
+    onClosed: function () {
+      this.dialogVisible = false
+      this.$emit('update:visible', false)
+      this.$refs.form.resetFields()
+      this.errors = []
+    },
+    save: function () {
+      this.$refs.form.validate(
+        valid => {
+          if (!valid) {
+            return false
+          }
+
+          this.errors = []
+          axios
+            .post(URL, {
+              new_password1: this.form.new_password1,
+              new_password2: this.form.new_password2
+            })
+            .then(
+              response => {
+                this.onClosed()
+              }
+            )
+            .catch(
+              error => {
+                const response = error.response.data
+                for (const key in response) {
+                  if (response.hasOwnProperty(key)) {
+                    this.errors.push(...response[key])
+                  }
+                }
+              }
+            )
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style>
+.el-alert {
+  margin-bottom: 20px;
+}
+</style>
--- Client/src/components/Header.vue
+++ Client/src/components/Header.vue
@@ -1,34 +1,74 @@
-/**
+/**]
  *  ヘッダーコンポーネント。
  */
 <template>
   <div>
     <div class="line"></div>
-      <el-menu :default-active="activeIndex1"
-        mode="horizontal"
-        @select="handleSelectIndex"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b">
+    <el-menu :default-active="activeIndex1"
+      mode="horizontal"
+      @select="handleSelectIndex"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
       <el-menu-item index="1">プロジェクト一覧</el-menu-item>
-      <el-menu-item index="2" class="loginuser">
-        ログインユーザー(アイコン)
-      </el-menu-item>
+      <el-submenu index="2" class="loginuser">
+        <template slot="title">{{ displayName }} さん</template>
+        <el-menu-item index="2-1">ユーザー情報編集</el-menu-item>
+        <el-menu-item index="2-2" @click="changePasswordFormVisible = true">パスワード変更</el-menu-item>
+        <el-menu-item index="2-3" @click="logout">ログアウト</el-menu-item>
+      </el-submenu>
     </el-menu>
+    <ChangePasswordForm :visible.sync="changePasswordFormVisible" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import ChangePasswordForm from './ChangePasswordForm'
+
+const URL_USER = process.env.API_BASE_URL + '/rest-auth/user/'
+const URL_LOGOUT = process.env.API_BASE_URL + '/rest-auth/logout/'
+
 export default {
   name: 'Header',
   data () {
     return {
+      user: null,
+      changePasswordFormVisible: false,
       activeIndex1: '1'
+    }
+  },
+  components: {
+    ChangePasswordForm
+  },
+  mounted () {
+    axios
+      .get(URL_USER)
+      .then(
+        response => {
+          this.user = response.data
+        }
+      )
+  },
+  computed: {
+    displayName () {
+      if (!this.user) {
+        return ''
+      }
+      if (this.user.last_name) {
+        return this.user.last_name + ' ' + this.user.first_name
+      } else {
+        return this.user.username
+      }
     }
   },
   methods: {
     handleSelectIndex (key, keyPath) {
       console.log(key, keyPath)
+    },
+    logout () {
+      axios.post(URL_LOGOUT)
+      this.$router.push({name: 'Login'})
     }
   }
 }
