5f46eca835067a82f9d5ecbdfe6b453705367220
webstry <kawajiri-yasuo@osscat.net>
2018-08-22 13:08:10 +0900

    プロジェクトに参加しているメンバーのみ表示されるよう修正

--- Client/src/components/SelectUser.vue
+++ Client/src/components/SelectUser.vue
@@ -21,30 +21,58 @@
 import axios from 'axios'
 
 const USER_URL = process.env.API_BASE_URL + '/api/users/'
+const PROJECTMEMBER_URL = process.env.API_BASE_URL + '/api/projectmembers/'
 
 export default {
   name: 'SelectUser',
-  props: ['selected'],
+  props: ['projectid', 'selected'],
   data () {
     return {
       users: [],
       value: null,
       list: [],
+      members: [],
       loading: false
     }
   },
   mounted () {
-    this.loadUser() // TODO:1回だけ。
+    this.loadProjectMembers() // TODO:1回だけ。
   },
   methods: {
+    loadProjectMembers: function () {
+      var url = PROJECTMEMBER_URL
+      if (this.projectid) {
+        url += ('?project_id=' + this.projectid)
+      }
+      axios
+        .get(url)
+        .then(
+          response => {
+            if (response.data && response.data.length > 0) {
+              for (let i = 0; i < response.data.length; i++) {
+                this.members.push(response.data[i].user_id)
+              }
+              if (this.members && this.members.length > 0) {
+                this.loadUser() // 全ユーザーを取得
+              }
+            } else {
+              console.log('プロジェクトメンバーがいません')
+            }
+          }
+        )
+    },
     loadUser: function () {
       axios
         .get(USER_URL)
         .then(
           response => {
-            this.list = response.data.map(user => {
-              return { value: user.id, label: user.username }
-            })
+            for (let i = 0; i < response.data.length; i++) {
+              var userid = response.data[i].id
+              var username = response.data[i].username
+              if (this.members.indexOf(userid) > -1) { // プロジェクトメンバーのみ
+                this.list.push({ value: userid, label: username })
+              }
+            }
             this.users = this.list
             if (this.selected) {
               this.value = this.selected
