f2cda790d836ed40239376fbed090755f4047cd8
aa140043 <masuda-kazuya@gr.sei.co.jp>
2018-09-12 14:18:40 +0900

    プロジェクト登録時にメンバーにオーナーを追加。オーナーが "3" 固定になってたので修正。

--- Client/src/components/AddProjectForm.vue
+++ Client/src/components/AddProjectForm.vue
@@ -13,27 +13,53 @@ import { EV_PROJECT } from '../plugins/WebSocket'
 import axios from 'axios'
 
 const PROJECT_POST_URL = process.env.API_BASE_URL + '/api/projects/'
+const MEMBER_URL = process.env.API_BASE_URL + '/api/projectmembers/'
+const URL_USER = process.env.API_BASE_URL + '/rest-auth/user/'
 
 export default {
   name: 'AddProjectForm',
   data () {
     return {
+      user: null,
       newtitle: null
     }
   },
   methods: {
+    addMember: function (projectId, userId) {
+      axios
+        .post(MEMBER_URL, {
+          project_id: projectId,
+          user_id: userId
+        })
+        .then(response => {
+          console.log(response)
+        })
+    },
     addProject: function () {
       axios
         .post(PROJECT_POST_URL, {
           title: this.newtitle,
-          owner_id: 3
+          owner_id: this.user.pk
         })
         .then(response => {
           this.$parent.projects.push(response.data)
           this.$webSocket.send(EV_PROJECT)
           this.newtitle = ''
+          this.addMember(response.data.id, this.user.pk)
         })
+    },
+    loadUser: function () {
+      axios
+        .get(URL_USER)
+        .then(
+          response => {
+            this.user = response.data
+          }
+        )
     }
+  },
+  mounted () {
+    this.loadUser()
   }
 }
 </script>
