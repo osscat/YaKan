fcb0614f03edbe5f02345efc43bdd28ca9943ecb
Naotaka Nakanishi <nakanishi-naotaka@osscat.net>
2018-09-12 13:56:46 +0900

    ユーザーが削除できない問題に対応

--- Docker/settings/settings.py
+++ Docker/settings/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_auth.registration',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'django_filters',
     'corsheaders',
     'YaKanSocket',
