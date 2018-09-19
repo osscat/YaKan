08934d4f1b6ef7d2a33e62154bf0d35892794e71
webstry <kawajiri-yasuo@osscat.net>
2018-09-19 17:02:21 +0900

    プロジェクトメンバーをstoreから取得するように変更

--- Client/src/components/SelectUser.vue
+++ Client/src/components/SelectUser.vue
@@ -3,100 +3,55 @@
  */
 <template>
   <div>
-    <el-select v-model="value" filterable clearable remote reserve-keyword placeholder="Select User"
+    <el-select v-model="id" filterable clearable remote reserve-keyword placeholder="Select User"
       :remote-method="remoteMethod"
       :loading="loading"
-      @change="updateuser">
+      @change="updateuser"
+      v-on:change="$emit('onSelect', id)">
       <el-option
-        v-for="user in users"
-        :key="user.value"
-        :label="user.label"
-        :value="user.value">
+        v-for="member in this.$store.getters.getMembers"
+        :key="member.id"
+        :label="member.user.username"
+        :value="member.id">
       </el-option>
     </el-select>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
-const USER_URL = process.env.API_BASE_URL + '/api/users/'
-const PROJECTMEMBER_URL = process.env.API_BASE_URL + '/api/projectmembers/'
-
 export default {
   name: 'SelectUser',
-  props: ['projectid', 'selected'],
+  props: ['selected'],
   data () {
     return {
-      users: [],
-      value: null,
-      list: [],
-      members: [],
+      id: null,
       loading: false
     }
   },
   mounted () {
-    this.loadProjectMembers() // TODO:1回だけ。
+    this.loadProjectMembers()
   },
   methods: {
     loadProjectMembers: function () {
-      var url = PROJECTMEMBER_URL
-      if (this.projectid) {
-        url += ('?project_id=' + this.projectid)
+      if (this.selected) {
+        this.id = this.selected
       }
-      axios
-        .get(url)
-        .then(
-          response => {
-            if (response.data && response.data.length > 0) {
-              for (let i = 0; i < response.data.length; i++) {
-                this.members.push(response.data[i].user_id)
-              }
-              if (this.members && this.members.length > 0) {
-                this.loadUser() // 全ユーザーを取得
-              }
-            } else {
-              console.log('プロジェクトメンバーがいません')
-            }
-          }
-        )
-    },
-    loadUser: function () {
-      axios
-        .get(USER_URL)
-        .then(
-          response => {
-            for (let i = 0; i < response.data.length; i++) {
-              var userid = response.data[i].id
-              var username = response.data[i].username
-              if (this.members.indexOf(userid) > -1) { // プロジェクトメンバーのみ
-                this.list.push({ value: userid, label: username })
-              }
-            }
-            this.users = this.list
-            if (this.selected) {
-              this.value = this.selected
-            }
-          }
-        )
     },
     updateuser: function () {
-      if (this.value === '') {
-        this.users = this.list
+      const isFunc = typeof (this.$parent.$parent.changeUser)
+      if (isFunc === 'function') {
+        this.$parent.$parent.changeUser(this.id)
       }
-      this.$parent.$parent.changeUser(this.value)
     },
     remoteMethod: function (query) {
       if (query !== '') {
         this.loading = true
         setTimeout(() => {
           this.loading = false
-          this.users = this.list.filter(user => {
+          this.$store.getters.getMembers.filter(user => {
             return user.label.toLowerCase().indexOf(query.toLowerCase()) > -1
           })
         }, 200)
-      } else {
-        this.users = this.list
       }
     }
   }
